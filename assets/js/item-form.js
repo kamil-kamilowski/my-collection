6e2534da5b531c76ae9bace68f7037a52a1541a6
Kamil Roczniok <kroczniok@gmail.com>
2018-01-23 20:44:03 +0100

    Use ES6

--- assets/js/item-form.js
+++ assets/js/item-form.js
@@ -1,25 +1,25 @@
-var $ = require('jquery');
+const $ = require('jquery');
+const $category = $('#item_category');
 
-var $category = $('#item_category');
-// When sport gets selected ...
-$category.change(function () {
-    // ... retrieve the corresponding form.
-    var $form = $(this).closest('form');
-    // Simulate form data, but only include the selected sport value.
-    var data = {};
+//on category change try to get genres for selected category
+$category.change(() => {
+
+    // init data
+    const $form = $category.closest('form'),
+        $item = $('#item_genre'),
+        data = {}; // init an empty object
+
+    // disable temporary genre selectbox
+    $item.attr('disabled', true);
+
+    // get category value
     data[$category.attr('name')] = $category.val();
-    // Submit data via AJAX to the form's action path.
+    console.log($category.val());
     $.ajax({
         url: $form.attr('action'),
         type: $form.attr('method'),
         data: data,
-        success: function (html) {
-            // Replace current position field ...
-            $('#item_genre').replaceWith(
-                // ... with the returned one from the AJAX response.
-                $(html).find('#item_genre')
-            );
-            // Position field now displays the appropriate positions.
-        }
+        success: (html) => $('#item_genre').replaceWith($(html).find('#item_genre')), // replace dom el. with selectbox from response
+        error: () => $item.attr('disabled', false) // on error enable genre again
     });
 });
